7424e7bde07d046722df5f769d660fa0bf6c22d4
Vinays <vinay@C02R453FGF1J.local>
2019-02-11 17:47:21 +0530

    new commit

---
+++ src/main/java/com/example/sumne/SumneApplication.java
@@ -0,0 +1,14 @@
+package com.example.sumne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumneApplication.class, args);
+	}
+
+}
+
